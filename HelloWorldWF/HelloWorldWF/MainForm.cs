7018b4e1fa32c17a42542d2b78ea7b50adb84f76
Toth Marcell <tothmc@gmail.com>
2022-03-26 23:30:20 +0100

    labor 03 elkeszitve

---
+++ HelloWorldWF/HelloWorldWF/MainForm.cs
@@ -0,0 +1,76 @@
+using System.Diagnostics;
+
+namespace HelloWorldWF
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bUpdateText_Click(object sender, EventArgs e)
+        {
+            tbDemoText.Text = "Hello";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            TreeNode root = treeView1.Nodes.Add("Local Disk (C:)");
+            root.Tag = new DirectoryInfo("C:\\");
+            root.Nodes.Add("");
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            DirectoryInfo parentDI = (DirectoryInfo)(e.Node.Tag);
+            e.Node.Nodes.Clear();
+            try
+            {
+                foreach (DirectoryInfo di in parentDI.GetDirectories())
+                {
+                    TreeNode node = new TreeNode(di.Name);
+                    node.Tag = di;
+                    node.Nodes.Add("");
+                    e.Node.Nodes.Add(node);
+                }
+            }
+            catch { }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            DirectoryInfo parentDI = (DirectoryInfo)(e.Node.Tag);
+            listView1.Items.Clear();
+            try
+            {
+                foreach (FileInfo fi in parentDI.GetFiles())
+                {
+                    listView1.Items.Add(new ListViewItem(new string[] { 
+                        fi.Name, fi.Length.ToString(), 
+                        fi.LastWriteTime.ToString(),
+                        fi.FullName}));
+                }
+            }
+            catch { }
+
+            tbDemoText.Text = parentDI.FullName;
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 1) return;
+            string fullName = listView1.SelectedItems[0].SubItems[3].Text;
+            if (fullName != null)
+                Process.Start(new ProcessStartInfo(fullName)
+                {
+                    UseShellExecute =   true
+                });
+        }
+    }
+}
